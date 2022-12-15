170250b4dffa209e3ce1999abd493def83dd3aeb
Iron Skull <ironskull89@mail.ru>
2022-12-15 06:28:23 +0300

    Add Example006_Base console project

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1;
+int b = 4;
+int c = 9;
+int d = 2;
+int e = 7;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("max = " + max);
