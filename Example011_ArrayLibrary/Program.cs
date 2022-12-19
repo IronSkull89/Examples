91ca99acd647b7c479c4fd17077edcf4ddd3c147
Iron Skull <ironskull89@mail.ru>
2022-12-19 05:13:05 +0300

    added Example011_ArrayLibrary console project

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,41 @@
+﻿void FillArray(int[] collection) {
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+      collection[index] = new Random().Next(1,10);  
+      index++;  
+    }
+}
+
+void PrintArray(int[] col) {
+    int length = col.Length;
+    int index = 0;
+    while (index < length)
+    {
+      Console.WriteLine(col[index]);  
+      index++;  
+    }
+}
+
+int IndexOf (int[] collection, int find) {
+    int length = collection.Length;
+    int position = -1;
+    int index = 0;
+    while (index < length)
+    {
+      if (collection[index] == find) {
+        position = index;
+        break;
+      }  
+      index++;
+    }
+    return position;
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine(IndexOf(array, 4));
