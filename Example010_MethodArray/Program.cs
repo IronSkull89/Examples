22b9c9fb496e5360ee0b086f821f15c9bf58bb61
Iron Skull <ironskull89@mail.ru>
2022-12-19 04:39:36 +0300

    added Example010_MethoArray console project

--- Example010_MethodArray/Program.cs
+++ Example010_MethodArray/Program.cs
@@ -1,10 +1,10 @@
 ﻿int[] array = {23, 65, 21, 80, 54, 40, 15, 10, 1};
 
 int find = 80;
-
 int index = 0;
+int length = array.Length;
 
-while(index < array.Length) {
+while(index < length) {
     if (array[index] == find) {
         Console.WriteLine(index);
         break;
