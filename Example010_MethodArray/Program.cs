4891f12a68f8f89e92c2de7eb672a0ca29f08b1c
Iron Skull <ironskull89@mail.ru>
2022-12-19 04:19:02 +0300

    added Example010_MethoArray console project

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {23, 65, 21, 80, 54, 40, 15, 10, 1};
+
+int find = 80;
+
+int index = 0;
+
+while(index < array.Length) {
+    if (array[index] == find) {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
